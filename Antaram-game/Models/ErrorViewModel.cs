ef57c3430ccdcf6fee808ad26265e6ba33b34d58
Juraj <j.lormusz@gmail.com>
2022-09-14 11:48:05 +0200

    Initial setup

---
+++ Antaram-game/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Antaram_game.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
